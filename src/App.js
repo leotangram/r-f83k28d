61942cb1d2d9a8527beffd757c04d78e9d7a7f33
leotangram <leotangram@gmail.com>
2018-02-02 11:42:58 -0500

    reto resuelto

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	constructor() {
+	  super();
+		this.changeNum = this.changeNum.bind(this);
+
+	  this.state = {
+	   num: 0
+	  };
+	}
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.num }</span>
+        <button onClick={this.changeNum} id="inc">Incrementa</button>
       </div>
     );
+  }
+
+  changeNum() {
+  	this.setState({
+  		num: this.state.num+=1
+  	});
   }
 }
 
